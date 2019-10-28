13d1c4a5c6343e4fa21b5b2b1d39144ca104f3a9
Alexey Egorov <alexey.ig.egorov@gmail.com>
2019-10-28 22:34:59 +0300

    Add new car form

--- app/spends/forms.py
+++ app/spends/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 from flask_babel import lazy_gettext as _l
-from wtforms import SelectField
-from wtforms.validators import InputRequired
+from wtforms import SelectField, StringField
+from wtforms.validators import InputRequired, DataRequired
 
 
 class AddCarForm(FlaskForm):
@@ -10,3 +10,11 @@ class AddCarForm(FlaskForm):
     fueltype = SelectField(_l('Fuel type'), coerce=int, validators=[InputRequired])
     enginevolume = SelectField(_l('Engine volume'), coerce=int, validators=[InputRequired])
     enginepower = SelectField(_l('Engine power'), coerce=int, validators=[InputRequired])
+
+
+class AddNewCarForm(FlaskForm):
+    manufacturer = StringField(_l('Manufacturer'), validators=[DataRequired])
+    model = StringField(_l('Manufacturer'), validators=[DataRequired])
+    fueltype = StringField(_l('Fuel type'), validators=[DataRequired])
+    enginevolume = StringField(_l('Engine volume'), validators=[DataRequired])
+    enginepower = StringField(_l('Engine power'), validators=[DataRequired])
