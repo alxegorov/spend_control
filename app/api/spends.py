4a613c6fcc66409ace06b292ed847c2b7c0a0605
Alexey Egorov <alexey.ig.egorov@gmail.com>
2020-02-06 23:04:19 +0300

    Get cars API

--- app/api/spends.py
+++ app/api/spends.py
@@ -1,6 +1,16 @@
 from app.api import bp
 from app.api.auth import token_auth
-from app.models import CarSpendType
+from app.models import CarSpendType, Car
+from flask import jsonify, g
+
+@bp.route('/spends/move/cars', methods=['GET'])
+@token_auth.login_required
+def get_cars():
+    data = []
+    cars = Car.query.filter(Car.user_id == g.current_user.id)
+    for car in cars:
+        data.append({'id': car.id, 'value': car.model.manufacturer + ' ' + car.model.model})
+    return jsonify(data)
 
 @bp.route('/spends/move/car/types', methods=['GET'])
 @token_auth.login_required
