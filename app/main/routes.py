f335c22fb0eb930c3eb3aa2f009b9e6f2a6f116d
Alexey <alexey.ig.egorov@gmail.com>
2020-04-21 23:25:12 +0300

    Add brand and favicon

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, redirect
 from flask_babel import _
 from app.main import bp
 
@@ -7,3 +7,8 @@ from app.main import bp
 @bp.route('/index')
 def index():
     return render_template('index.html', title=_('Home'))
+
+
+@bp.route('/favicon.ico')
+def favicon():
+    return redirect('https://s.gravatar.com/avatar/f9eca48d6c9afa6dbeeefc3484b4985d?s=80')
