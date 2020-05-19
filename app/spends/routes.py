0e06c572c4bb07a3ee32a0e6fdeb20b3e0eb71ea
Alexey <alexey.ig.egorov@gmail.com>
2020-05-19 21:15:57 +0300

    Pugination bug

--- app/spends/routes.py
+++ app/spends/routes.py
@@ -55,8 +55,8 @@ def index():
     page = request.args.get('page', 1, type=int)
     spends = CarSpend.query.filter(CarSpend.car_id.in_(car_id)).order_by(CarSpend.timestamp.desc()).paginate(page, 
         current_app.config['POSTS_PER_PAGE'], False)
-    next_url = url_for('spends.addcarspend', car_id=car_id, page=spends.next_num) if spends.has_next else None
-    prev_url = url_for('spends.addcarspend', car_id=car_id, page=spends.prev_num) if spends.has_prev else None
+    next_url = url_for('spends.index', car_id=car_id, page=spends.next_num) if spends.has_next else None
+    prev_url = url_for('spends.index', car_id=car_id, page=spends.prev_num) if spends.has_prev else None
     return render_template('spends.html', title=_('Spends'), spends=spends.items, next_url=next_url, 
         prev_url=prev_url, add_spend_form=add_spend_form, add_spend_type_form=add_spend_type_form, cars=cars,
         add_new_car_form=add_new_car_form, add_car_form=add_car_form)
